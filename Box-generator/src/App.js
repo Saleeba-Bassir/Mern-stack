d80f2f4ef64ec319b64178c04445c2820e25e56e
Saleeba-Bassir <124049823+Saleeba-Bassir@users.noreply.github.com>
2023-06-11 20:49:40 +0300

    Add files via upload

---
+++ Box-generator/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import BoxCreator from './component/BoxCreator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Create Boxes</h1>
+      <BoxCreator />
+    </div>
+  );
+}
+
+export default App;
+
